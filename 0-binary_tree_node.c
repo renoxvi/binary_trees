4cae61f4543166672587e98f7bc63df31f70939b
renoxvi <vicreno08@gmail.com>
2023-09-21 16:16:23 -0700

    ma

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,16 +4,15 @@
  * @parent: pointer to parent
  * Return: pointer to the new node
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-{
-binary_tree_t *nodo;
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+  binary_tree_t *nodo;
 
-nodo = malloc(sizeof(binary_tree_t));
-if (nodo == NULL)
-return (NULL);
-nodo->n = value;
-nodo->parent = parent;
-nodo->left = NULL;
-nodo->right = NULL;
-return (nodo);
+  nodo = malloc(sizeof(binary_tree_t));
+  if (nodo == NULL)
+    return (NULL);
+  nodo->n = value;
+  nodo->parent = parent;
+  nodo->left = NULL;
+  nodo->right = NULL;
+  return (nodo);
 }
